dd9a1114b7ddd078b8283b416d0d86da3e56ab5b
Arsen Stakhiv <arsen.stakhiv2305@gmail.com>
2025-07-30 23:28:38 +0300

    add: finish section 1 of chapter 19 with exercises

---
+++ 2_Advanced_Features_of_C/19_Program_Design/exercises/queue.h
@@ -0,0 +1,27 @@
+/************************************************************
+ * insert: insert an item to the begining of the queue.		*
+ ************************************************************/
+void insert(int value);
+
+/************************************************************
+ * remove: removes an item from the end of the queue		*
+ ************************************************************/
+int remove(void);
+
+/************************************************************
+ * get_first: returns the first item in the queue without	*
+ *			  removing it from the queue.					*
+ ************************************************************/
+int get_first(void);
+
+/************************************************************
+ * get_last: returns the last item in the queue without		*
+ *			 removing it from the queue.					*
+ ************************************************************/
+int get_last(void);
+
+/************************************************************
+ * is_empty: returns true if the queue is empty, false 		*
+ *			 otherwise.										*
+ ************************************************************/
+bool is_empty(void);
